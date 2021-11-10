fd25a8de620d00de684d60695e67115581ea4c30
Jhonny Rafael <rafaeljhonny01@gmail.com>
2021-11-09 22:48:13 -0300

    add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
-import { Button, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert, Button, SafeAreaView, StyleSheet } from 'react-native';
 import Header from './components/Header';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
   const [name, setName] = useState<string>('Jhonny');
-  const [title] = useState<string>('Olá');
+  const [title, setTitle] = useState<string>('Olá');
 
-
+/**
+ * Effect
+ */
+  useEffect(()=>{
+    console.log("mounting");
+    if(name == 'Jhonny'){
+      setTitle('Hello');
+    }else{
+      setTitle('Olá');
+    }
+    
+    return () => Alert.alert('OOPS', 'Component unmounted!')
+  }, [name, title]);
   /**
    * callback
    */
